7562fec0c1afdb17a6599fb5e9c36dccee732e15
Rido <daniridao@hotmail.com>
2017-03-25 12:06:57 +0000

    settings updated

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0w15!0d6w#eu$7#*ii#qnhxsjzz^y)-dy&%k%)enyo11mqhm*j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.4']
+ALLOWED_HOSTS = ['192.168.0.4', 'localhost', '127.0.0.1']
 
 
 # Application definition
