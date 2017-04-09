6fbc20e89a0477f3c14d9541e8e7b0a60327dd2a
Ridote <daniridao@gmail.com>
2017-04-09 20:53:55 +0100

    some changing in shoppinglist, just trying some stuff

--- mysite/shoppinglist/urls.py
+++ mysite/shoppinglist/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls import url
 from . import views
 app_name='shoppinglist'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^login$', views.LoginView.as_view(), name='login'),
-    url(r'^newProduct/$', views.NewProductView.as_view(), name='newProduct'),
-    url(r'^addProduct/$', views.addProduct, name='addProduct'),
-    url(r'^loginProcess/$', views.loginProcess, name='loginProcess'),
+    url(r'^$', views.index, name='index'),
+    url(r'^products/', views.viewProduct, name='product'),
 ]
--- mysite/shoppinglist/views.py
+++ mysite/shoppinglist/views.py
@@ -1,49 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views import generic
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+import datetime
+from .models import Product
+from django.contrib.auth.models import User
 
-from .models import Product, User
+def index(request):
+	today = datetime.datetime.now().date()
+	return render(request, "shoppinglist/index.html", {"today": today})
 
-class LoginView(generic.ListView):
-    template_name = 'shoppinglist/login.html'
-    context_object_name = 'credentials'
-    def get_queryset(self):
-        return None
-def loginProcess(request, username_str, password_str):
-	user = get_object_or_404(User, username = username_str)
-	print(user)
-	
-	'''
-	return render(request, 'polls/detail.html', {
-			'question': question,
-			'error_message': "You didn't select a choice.",
-			})
-	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-	'''
-
-
-class IndexView(generic.ListView):
-    template_name = 'shoppinglist/index.html'
-    context_object_name = 'products'
-    def get_queryset(self):
-        return Product.objects.order_by('-id')
-class NewProductView(generic.ListView):
-	template_name = 'shoppinglist/newProduct.html'
-	context_object_name = 'userObject'
-	def get_queryset(self):
-		pk = 2
-		return User.objects.filter(user_id=1)
-def newProduct(request):
-	user = get_object_or_404(User, pk=2)
-	print(user.username)
-	context = {'userObject': user}
-	return render(request, 'shoppinglist/newProduct.html', context)
-def addProduct(request):
-	quantity = request.POST.get("quantity", "")
-	name = request.POST.get("name", "")
-	requester = request.POST.get("requesterId", "")
-	user = get_object_or_404(User, pk=requester)
-	context = {'addingProduct': True, 'userObject': user, 'quantity': quantity, 'name': name}
-	
-	return render(request, 'shoppinglist/newProduct.html', context)
+def viewProduct(request):
+   me = User.objects.get(username='rido')
+   products = Product.objects.filter(requester = me)
+   return render(request, "shoppinglist/viewProducts.html", {"products": products})
